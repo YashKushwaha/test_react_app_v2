b6b5a69eb6166730e32cf9b36b19e3ccfe4487d5
Yash Kushwaha <37941798+YashKushwaha@users.noreply.github.com>
2025-12-26 15:45:09 +0530

    Branch Reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
-
-
-
-import {Header} from "@/components/Header"
-import {Sidebar} from "@/components/Sidebar"
-import {MainContent} from "@/components/MainContent"
-
 function App() {
   return (
-    <div className="h-screen w-screen flex flex-col">
-          {/* Top Bar */}
-
-      <Header />
-      <div className="flex flex-1 overflow-hidden">
-        <Sidebar />
-        <MainContent />
-      </div>
-    </div>
+    <>HELLO</>
   )
 }
 
